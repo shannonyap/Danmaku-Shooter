b076977e3b82f98bc5e9439b90973d05925d37a1
Shannon Yap <shannonyap@utexas.edu>
2017-03-15 18:50:55 -0500

    Added scrolling background.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+var spacefield;
 
 function preload() {
+  game.load.image('starfield', 'assets/starfield.jpg');
 }
 
 function create() {
+  spacefield = game.add.tileSprite(0,0,800,600, 'starfield');
 }
 
 function update() {
+  spacefield.tilePosition.y += 5;
 }
